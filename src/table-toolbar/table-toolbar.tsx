70381c7ff33235e04e5e34004cfb92b9f43d4c81
bane.shi <fengxiaotx@163.com>
2023-04-22 11:43:46 +0800

    feat: table-toolbar.tsx

--- src/table-toolbar/table-toolbar.tsx
+++ src/table-toolbar/table-toolbar.tsx
@@ -1,59 +1,63 @@
-import React, {FC} from "react";
+import { CSSInterpolation, useStyleRegister } from '@ant-design/cssinjs';
+import { RedoOutlined } from '@ant-design/icons';
+import { Button, GlobalToken, Space, theme, Tooltip } from 'antd';
 import classNames from 'classnames';
-import {Button, GlobalToken, Space, theme, Tooltip} from "antd";
-import _ from "lodash";
-import {RedoOutlined} from "@ant-design/icons";
-import {CSSInterpolation, useStyleRegister} from "@ant-design/cssinjs";
+import _ from 'lodash';
+import React, { FC } from 'react';
 
-const {useToken} = theme;
+const { useToken } = theme;
 const genTableToolbarStyle = (
   prefixCls: string,
   token: GlobalToken,
 ): CSSInterpolation => {
-  return ({
+  return {
     [`.${prefixCls}`]: {
-      display: 'flex', justifyContent: 'space-between', paddingBlock: '16px',
+      display: 'flex',
+      justifyContent: 'space-between',
+      paddingBlock: '16px',
       [`&-title`]: {
-        display: 'flex', justifyContent: 'flex-start'
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
       },
       [`&-tools`]: {
-        display: 'flex', justifyContent: 'flex-end'
-      }
+        display: 'flex',
+        justifyContent: 'flex-end',
+      },
     },
-  });
-}
+  };
+};
 
 interface TableToolbarProps {
-  className?: string,
+  className?: string;
   /**
    * @description 标题
    * @default null
    */
-  title?: React.ReactNode,
+  title?: React.ReactNode;
   /**
    * @description 工具
    * @default []
    */
-  tools?: React.ReactNode[] | React.ReactElement[]
+  tools?: React.ReactNode[] | React.ReactElement[];
   /**
    * @description 刷新回调
    * @default ()=>void
    */
-  onRefresh?: () => void
+  onRefresh?: () => void;
 }
 
 const TableToolbar: FC<TableToolbarProps> = ({
-                                               className, title, tools = [],
-                                               onRefresh
-                                             }) => {
-
-  const prefixCls = "ant-table-toolbar"
-  const {theme, token, hashId} = useToken();
+  className,
+  title,
+  tools = [],
+  onRefresh,
+}) => {
+  const prefixCls = 'ant-table-toolbar';
+  const { theme, token, hashId } = useToken();
   const wrapSSR = useStyleRegister(
-    {theme, token, hashId, path: [prefixCls]},
-    () => [
-      genTableToolbarStyle(prefixCls, token)
-    ],
+    { theme, token, hashId, path: [prefixCls] },
+    () => [genTableToolbarStyle(prefixCls, token)],
   );
 
   return wrapSSR(
@@ -61,16 +65,16 @@ const TableToolbar: FC<TableToolbarProps> = ({
       <div className={classNames(`${prefixCls}-title`, hashId)}>{title}</div>
       <div className={classNames(`${prefixCls}-tools`, hashId)}>
         <Space>
-          {!_.isEmpty(tools) && tools!.map(tool => {
-            return tool
-          })}
+          {!_.isEmpty(tools) &&
+            tools!.map((tool) => {
+              return tool;
+            })}
           <Tooltip title={`刷新`}>
-            <Button type={`text`} icon={<RedoOutlined/>} onClick={onRefresh}/>
+            <Button type={`text`} icon={<RedoOutlined />} onClick={onRefresh} />
           </Tooltip>
-
         </Space>
       </div>
-    </div>
-  )
-}
-export default TableToolbar
+    </div>,
+  );
+};
+export default TableToolbar;
