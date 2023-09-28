7b1a4b531a7f4fa2d9d194d9c4e3b48b0d331f3d
bane.shi <fengxiaotx@163.com>
2023-09-28 22:51:06 +0800

    []

--- src/layout/styles.ts
+++ src/layout/styles.ts
@@ -7,27 +7,28 @@ export const genLayoutStyle = (
 ): CSSInterpolation => {
   return {
     [`.${prefixCls}`]: {
-      minWidth:0,
-      minHeight:0,
+      minWidth: 0,
+      minHeight: 0,
       height: '100%',
       width: '100%',
       display: 'flex',
+      boxSizing: 'border-box',
       [`&-vertical`]: {
         flexDirection: 'column',
         // width:0,
         // flex:'1 1 auto'
-        [`>.${prefixCls}-item-auto`]:{
-          minWidth:0,
-          minHeight:0,
+        [`>.${prefixCls}-item-auto`]: {
+          minWidth: 0,
+          minHeight: 0,
         }
       },
       [`&-horizontal`]: {
         flexDirection: 'row',
         // height:0,
         // flex:'1 1 auto'
-        [`>.${prefixCls}-item-auto`]:{
-          minWidth:0,
-          minHeight:0,
+        [`>.${prefixCls}-item-auto`]: {
+          minWidth: 0,
+          minHeight: 0,
         }
       },
     }
@@ -41,9 +42,10 @@ export const genItemStyle = (
   return {
     [`.${prefixCls}`]: {
       [`&-auto`]: {
+        boxSizing: 'border-box',
         flex: '1 auto',
-        minWidth:0,
-        minHeight:0,
+        minWidth: 0,
+        minHeight: 0,
       }
     }
   }
@@ -55,8 +57,11 @@ export const genSiderStyle = (
 ): CSSInterpolation => {
   return {
     [`.${prefixCls}`]: {
+      boxSizing: 'border-box',
       height: '100%',
       display: 'flex',
+      minWidth: 0,
+      minHeight: 0,
       flexDirection: 'column',
       transition: 'all 0.2s,background 0s',
       [`&-children`]: {
