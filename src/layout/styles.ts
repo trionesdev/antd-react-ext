e89e8b633c6b730cf772c11fca64ff369d818295
bane.shi <fengxiaotx@163.com>
2024-04-10 22:16:20 +0800

    feat: add antd menu style in sider

--- src/layout/styles.ts
+++ src/layout/styles.ts
@@ -69,6 +69,9 @@ export const genSiderStyle = (
       [`&-children`]: {
         flex: '1 auto',
         overflowY: 'auto',
+        [`.ant-menu`]: {
+          borderInlineEnd: '0px !important',
+        },
       },
       [`&-trigger`]: {
         textAlign: 'center',
