0e265147ee5b53cd9ebba3f91c056a94196b0374
fengxiaotx <fengxiaotx@users.noreply.github.com>
2024-09-15 14:58:57 +0000

    deploy: e36c5388d47e0ac595e371936c3d92411e1c0eef

---
+++ meta__modal-form.8031d3e3.async.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunk_trionesdev_antd_react_ext=self.webpackChunk_trionesdev_antd_react_ext||[]).push([[914],{8008:function(o,t,e){e.r(t),e.d(t,{demos:function(){return l}});var r=e(97983),a=e.n(r),s=e(40794),u=e.n(s),d=e(67294),l={"modal-form-demo-base":{component:d.memo(d.lazy(function(){return e.e(422).then(e.bind(e,57641))})),asset:{type:"BLOCK",id:"modal-form-demo-base",refAtomIds:["modal-form"],dependencies:{"index.tsx":{type:"FILE",value:e(50487).Z},"@trionesdev/antd-react-ext":{type:"NPM",value:"0.0.7-beta.0"},antd:{type:"NPM",value:"5.18.0"},react:{type:"NPM",value:"18.2.0"}},entry:"index.tsx"},context:{"@trionesdev/antd-react-ext":e(72590),antd:e(54441),react:e(67294)},renderOpts:{compile:function(){var _=u()(a()().mark(function c(){var m,f=arguments;return a()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(944).then(e.bind(e,99944));case 2:return n.abrupt("return",(m=n.sent).default.apply(m,f));case 3:case"end":return n.stop()}},c)}));function i(){return _.apply(this,arguments)}return i}()}}}},90749:function(o,t,e){e.r(t),e.d(t,{texts:function(){return r}});const r=[{value:"\u6A21\u6001\u6846\u8868\u5355",paraId:0,tocIndex:0}]},50487:function(o,t){t.Z=`import { ModalForm } from '@trionesdev/antd-react-ext';
+import { Button, Form, Input } from 'antd';
+import React from 'react';
+
+export default () => (
+  <div>
+    <ModalForm trigger={<Button>\u65B0\u5EFA</Button>} title={\`\u8868\u5355\`}>
+      <Form.Item label={\`\u59D3\u540D\`} name={\`name\`}>
+        <Input />
+      </Form.Item>
+    </ModalForm>
+  </div>
+);
+`}}]);
