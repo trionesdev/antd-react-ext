6883774debaf795b75fbd988a80a36313acbefe4
fengxiaotx <fengxiaotx@users.noreply.github.com>
2024-10-09 08:40:51 +0000

    deploy: fed5ee2693e287592fce1eea82385382bbf01815

---
+++ meta__drawer-form.35799626.async.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunk_trionesdev_antd_react_ext=self.webpackChunk_trionesdev_antd_react_ext||[]).push([[2899],{33123:function(d,t,e){var r;e.r(t),e.d(t,{demos:function(){return i}});var _=e(15009),o=e.n(_),s=e(99289),u=e.n(s),a=e(67294),D=e(98046),l=e(62760),c=e(73453),i={"drawer-form-demo-base":{component:a.memo(a.lazy(function(){return e.e(1341).then(e.bind(e,98635))})),asset:{type:"BLOCK",id:"drawer-form-demo-base",refAtomIds:["drawer-form"],dependencies:{"index.tsx":{type:"FILE",value:e(92984).Z},"@trionesdev/antd-react-ext":{type:"NPM",value:"0.0.7-beta.3"},antd:{type:"NPM",value:"5.21.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@trionesdev/antd-react-ext":l,antd:c,react:r||(r=e.t(a,2))},renderOpts:{compile:function(){var E=u()(o()().mark(function f(){var m,x=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(m=n.sent).default.apply(m,x));case 3:case"end":return n.stop()}},f)}));function v(){return E.apply(this,arguments)}return v}()}}}},26920:function(d,t,e){e.r(t),e.d(t,{texts:function(){return _}});var r=e(98046);const _=[{value:"\u62BD\u5C49\u8868\u5355",paraId:0,tocIndex:0}]},92984:function(d,t){t.Z=`import { DrawerForm } from '@trionesdev/antd-react-ext';
+import { Button, Form, Input } from 'antd';
+import React from 'react';
+
+export default () => (
+  <div>
+    <DrawerForm trigger={<Button>\u65B0\u5EFA</Button>} title={\`\u8868\u5355\`}>
+      <Form.Item label={\`\u59D3\u540D\`} name={\`name\`}>
+        <Input />
+      </Form.Item>
+    </DrawerForm>
+  </div>
+);
+`}}]);
