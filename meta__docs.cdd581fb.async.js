3700bd0d4889da9bae9e7d85dc3e4908033dd050
fengxiaotx <fengxiaotx@users.noreply.github.com>
2024-10-24 10:05:35 +0000

    deploy: 23dae63378593d2fc7f13da81e871e28bbdaba09

--- meta__docs.cdd581fb.async.js
+++
@@ -1,5 +0,0 @@
-"use strict";(self.webpackChunk_trionesdev_antd_react_ext=self.webpackChunk_trionesdev_antd_react_ext||[]).push([[1904],{60810:function(a,n,t){t.r(n),t.d(n,{demos:function(){return _}});var e=t(67294),d=t(25223),_={}},11171:function(a,n,t){t.r(n),t.d(n,{demos:function(){return _}});var e=t(67294),d=t(68537),_={}},87799:function(a,n,t){t.r(n),t.d(n,{texts:function(){return d}});var e=t(25223);const d=[{value:"\u8BE5\u9879\u76EE\u662F\u57FA\u4E8E antd \u7684\u4E00\u4E2A\u6269\u5C55\u7EC4\u4EF6\u5E93",paraId:0,tocIndex:1},{value:`\u6DFB\u52A0.npmrc \u6587\u4EF6
-\u5185\u5BB9`,paraId:1,tocIndex:2},{value:`registry=https://moensun-npm.pkg.coding.net/npm/moensun/
-`,paraId:2,tocIndex:2},{value:`npm install @trionesdev/antd-react-ext@latest
-`,paraId:3,tocIndex:3},{value:`yarn add @trionesdev/antd-react-ext@latest
-`,paraId:4,tocIndex:3}]},51446:function(a,n,t){t.r(n),t.d(n,{texts:function(){return d}});var e=t(68537);const d=[{value:"antd \u7EC4\u4EF6\u6269\u5C55\u5E93",paraId:0}]}}]);
