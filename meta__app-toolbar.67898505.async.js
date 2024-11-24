aa3b8563b8176b4fa5baa76ec5565eaef10d4544
fengxiaotx <fengxiaotx@users.noreply.github.com>
2024-11-24 07:00:57 +0000

    deploy: 7733d49a16b49bb697726098d92eba43a5ab77e1

---
+++ meta__app-toolbar.67898505.async.js
@@ -0,0 +1,34 @@
+"use strict";(self.webpackChunk_trionesdev_antd_react_ext=self.webpackChunk_trionesdev_antd_react_ext||[]).push([[9250],{87067:function(o,t,e){var a;e.r(t),e.d(t,{demos:function(){return c}});var _=e(15009),d=e.n(_),l=e(99289),m=e.n(l),r=e(67294),M=e(95218),u=e(62760),i=e(33939),c={"app-toolbar-demo-base":{component:r.memo(r.lazy(function(){return e.e(148).then(e.bind(e,71437))})),asset:{type:"BLOCK",id:"app-toolbar-demo-base",refAtomIds:["app-toolbar"],dependencies:{"index.tsx":{type:"FILE",value:e(40866).Z},"@trionesdev/antd-react-ext":{type:"NPM",value:"0.0.7-beta.6"},antd:{type:"NPM",value:"5.22.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@trionesdev/antd-react-ext":u,antd:i,react:a||(a=e.t(r,2))},renderOpts:{compile:function(){var p=m()(d()().mark(function v(){var s,x=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(s=n.sent).default.apply(s,x));case 3:case"end":return n.stop()}},v)}));function E(){return p.apply(this,arguments)}return E}()}}}},1384:function(o,t,e){e.r(t),e.d(t,{texts:function(){return _}});var a=e(95218);const _=[{value:"\u5E94\u7528\u5DE5\u5177\u680F",paraId:0,tocIndex:0}]},40866:function(o,t){t.Z=`import { AppToolbar } from '@trionesdev/antd-react-ext';
+import { Button } from 'antd';
+import React from 'react';
+
+export default () => {
+  const items = [
+    {
+      key: 'user',
+      label: '\u7528\u6237',
+    },
+    {
+      key: 'permission',
+      label: '\u6743\u9650',
+    },
+    {
+      key: '1',
+      label: '\u529F\u80FD\u6E05\u5355',
+    },
+  ];
+  return (
+    <div>
+      <AppToolbar
+        title={'\u4E2A\u4EBA\u4E2D\u5FC3'}
+        navItems={items}
+        extra={[
+          <Button key={\`settings-btn\`} type={\`primary\`}>
+            \u8BBE\u7F6E
+          </Button>,
+        ]}
+      />
+    </div>
+  );
+};
+`}}]);
---
+++ meta__grid-table.be5e5ee4.async.js
@@ -0,0 +1,73 @@
+"use strict";(self.webpackChunk_trionesdev_antd_react_ext=self.webpackChunk_trionesdev_antd_react_ext||[]).push([[8273],{51054:function(m,t,e){var r;e.r(t),e.d(t,{demos:function(){return p}});var s=e(15009),o=e.n(s),v=e(99289),c=e.n(v),a=e(67294),f=e(82702),g=e(62760),p={"grid-table-demo-base":{component:a.memo(a.lazy(function(){return e.e(2172).then(e.bind(e,86217))})),asset:{type:"BLOCK",id:"grid-table-demo-base",refAtomIds:["grid-table"],dependencies:{"index.tsx":{type:"FILE",value:e(63761).Z},"@trionesdev/antd-react-ext":{type:"NPM",value:"0.0.7-beta.6"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@trionesdev/antd-react-ext":g,react:r||(r=e.t(a,2))},renderOpts:{compile:function(){var l=c()(o()().mark(function _(){var d,i=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(d=n.sent).default.apply(d,i));case 3:case"end":return n.stop()}},_)}));function u(){return l.apply(this,arguments)}return u}()}},"grid-table-demo-fit-table":{component:a.memo(a.lazy(function(){return e.e(2172).then(e.bind(e,29876))})),asset:{type:"BLOCK",id:"grid-table-demo-fit-table",refAtomIds:["grid-table"],dependencies:{"index.tsx":{type:"FILE",value:e(37373).Z},"@trionesdev/antd-react-ext":{type:"NPM",value:"0.0.7-beta.6"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@trionesdev/antd-react-ext":g,react:r||(r=e.t(a,2))},renderOpts:{compile:function(){var l=c()(o()().mark(function _(){var d,i=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(d=n.sent).default.apply(d,i));case 3:case"end":return n.stop()}},_)}));function u(){return l.apply(this,arguments)}return u}()}}}},58731:function(m,t,e){e.r(t),e.d(t,{texts:function(){return s}});var r=e(82702);const s=[{value:"\u8868\u683C",paraId:0,tocIndex:0},{value:"\u6491\u6EE1\u4E00\u4E2A\u9AD8\u5EA6\u4E3A300px\u7684\u5BB9\u5668\uFF0C\u5185\u90E8\u9AD8\u5EA6\u6EDA\u52A8",paraId:1}]},63761:function(m,t){t.Z=`import { GridTable } from '@trionesdev/antd-react-ext';
+import React from 'react';
+
+export default () => {
+  const columns = [
+    {
+      title: '\u59D3\u540D',
+      dataIndex: 'name',
+    },
+    {
+      title: '\u5E74\u9F84',
+      dataIndex: 'age',
+    },
+  ];
+
+  const dataScore = [
+    { name: '\u5C0F\u660E', age: 19 },
+    { name: '\u5C0F\u738B', age: 38 },
+  ];
+
+  return (
+    <div>
+      <GridTable columns={columns} dataSource={dataScore} />
+    </div>
+  );
+};
+`},37373:function(m,t){t.Z=`import { GridTable } from '@trionesdev/antd-react-ext';
+import React from 'react';
+
+export default () => {
+  const columns = [
+    {
+      title: '\u59D3\u540D',
+      dataIndex: 'name',
+    },
+    {
+      title: '\u5E74\u9F84',
+      dataIndex: 'age',
+    },
+  ];
+
+  const dataScore = [
+    { name: '\u5C0F\u660E', age: 19 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+    { name: '\u5C0F\u738B', age: 38 },
+  ];
+
+  return (
+    <div style={{ height: 300 }}>
+      <GridTable fit={true} columns={columns} dataSource={dataScore} />
+    </div>
+  );
+};
+`}}]);
