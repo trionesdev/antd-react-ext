9c0eb056bb848a7145af69090707ed034298dcdd
落雁沙 <fengxiaotx@163.com>
2024-11-30 18:36:24 +0800

    fit: add ReactDomUtils ,user can create React element by static function

--- src/util/ReactDomUtils.tsx
+++ src/util/ReactDomUtils.tsx
@@ -1,22 +1,25 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 export class ReactDomUtils {
-  static render(children:React.ReactElement,container:HTMLElement){
+  static render(children: React.ReactElement, container?: HTMLElement) {
     const div = document.createElement('div');
-    const body = container || document.body
-    body.appendChild(div)
+    const body = container || document.body;
+    body.appendChild(div);
 
-    const root = ReactDOM.createRoot(
-      div as HTMLElement
-    );
+    const root = ReactDOM.createRoot(div as HTMLElement);
 
     function destroy() {
-      setTimeout(() => root.unmount())
+      setTimeout(() => root.unmount());
       if (div.parentNode) {
         div.parentNode.removeChild(div);
       }
     }
-    root.render(React.cloneElement(children as React.ReactElement,{...children!.props,onDestroy:destroy}))
+    root.render(
+      React.cloneElement(children as React.ReactElement, {
+        ...children!.props,
+        onDestroy: destroy,
+      }),
+    );
   }
 }
