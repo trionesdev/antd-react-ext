d111c5b28bbeac2aa45804a4eac8efc948f0140e
fengxiaotx <fengxiaotx@users.noreply.github.com>
2025-05-27 05:42:53 +0000

    deploy: 302732597e6b2a09a8388366b13cc13dbb819346

---
+++ meta__app-toolbar.64f9c53b.async.js
@@ -0,0 +1,34 @@
+"use strict";(self.webpackChunk_trionesdev_antd_react_ext=self.webpackChunk_trionesdev_antd_react_ext||[]).push([[9250],{87067:function(o,n,e){var a;e.r(n),e.d(n,{demos:function(){return c}});var d=e(15009),_=e.n(d),u=e(99289),l=e.n(u),r=e(67294),m=e(77775),i=e(42025),c={"app-toolbar-demo-base":{component:r.memo(r.lazy(function(){return e.e(148).then(e.bind(e,71437))})),asset:{type:"BLOCK",id:"app-toolbar-demo-base",refAtomIds:["app-toolbar"],dependencies:{"index.tsx":{type:"FILE",value:e(40866).Z},"@trionesdev/antd-react-ext":{type:"NPM",value:"0.0.7-beta.16"},antd:{type:"NPM",value:"5.25.3"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@trionesdev/antd-react-ext":m,antd:i,react:a||(a=e.t(r,2))},renderOpts:{compile:function(){var p=l()(_()().mark(function E(){var s,f=arguments;return _()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(7335).then(e.bind(e,37335));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,f));case 3:case"end":return t.stop()}},E)}));function v(){return p.apply(this,arguments)}return v}()}}}},1384:function(o,n,e){e.r(n),e.d(n,{texts:function(){return a}});const a=[{value:"\u5E94\u7528\u5DE5\u5177\u680F",paraId:0,tocIndex:0}]},40866:function(o,n){n.Z=`import { AppToolbar } from '@trionesdev/antd-react-ext';
+import { Button } from 'antd';
+import React from 'react';
+
+export default () => {
+  const items = [
+    {
+      key: 'user',
+      label: '\u7528\u6237',
+    },
+    {
+      key: 'permission',
+      label: '\u6743\u9650',
+    },
+    {
+      key: '1',
+      label: '\u529F\u80FD\u6E05\u5355',
+    },
+  ];
+  return (
+    <div>
+      <AppToolbar
+        title={'\u4E2A\u4EBA\u4E2D\u5FC3'}
+        navItems={items}
+        extra={[
+          <Button key={\`settings-btn\`} type={\`primary\`}>
+            \u8BBE\u7F6E
+          </Button>,
+        ]}
+      />
+    </div>
+  );
+};
+`}}]);
---
+++ meta__avatar-editor.46d18881.async.js
@@ -0,0 +1,24 @@
+"use strict";(self.webpackChunk_trionesdev_antd_react_ext=self.webpackChunk_trionesdev_antd_react_ext||[]).push([[8743],{13206:function(o,e,t){var a;t.r(e),t.d(e,{demos:function(){return v}});var s=t(15009),d=t.n(s),u=t(99289),m=t.n(u),r=t(67294),l=t(77775),i=t(42025),v={"avatar-editor-demo-base":{component:r.memo(r.lazy(function(){return t.e(1728).then(t.bind(t,57584))})),asset:{type:"BLOCK",id:"avatar-editor-demo-base",refAtomIds:["avatar-editor"],dependencies:{"index.tsx":{type:"FILE",value:t(53157).Z},"@trionesdev/antd-react-ext":{type:"NPM",value:"0.0.7-beta.16"},antd:{type:"NPM",value:"5.25.3"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@trionesdev/antd-react-ext":l,antd:i,react:a||(a=t.t(r,2))},renderOpts:{compile:function(){var c=m()(d()().mark(function p(){var _,f=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(7335).then(t.bind(t,37335));case 2:return n.abrupt("return",(_=n.sent).default.apply(_,f));case 3:case"end":return n.stop()}},p)}));function E(){return c.apply(this,arguments)}return E}()}}}},14554:function(o,e,t){t.r(e),t.d(e,{texts:function(){return a}});const a=[{value:"\u5934\u50CF\u7F16\u8F91\u5668\uFF0C\u5E26\u56FE\u7247\u88C1\u526A\u529F\u80FD\u7684\u5934\u50CF\u7EC4\u4EF6",paraId:0,tocIndex:0}]},53157:function(o,e){e.Z=`import { AvatarEditor } from '@trionesdev/antd-react-ext';
+import { Button } from 'antd';
+import React from 'react';
+
+export default () => {
+  const [avatar, setAvatar] = React.useState<string>('');
+
+  return (
+    <div>
+      <AvatarEditor />
+      <Button
+        onClick={() =>
+          setAvatar(
+            'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+          )
+        }
+      >
+        \u8BBE\u7F6E\u56FE\u7247
+      </Button>
+      <AvatarEditor value={avatar} onChange={setAvatar} />
+    </div>
+  );
+};
+`}}]);
