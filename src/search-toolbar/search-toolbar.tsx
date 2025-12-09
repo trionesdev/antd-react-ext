b9444e5cb19bf82db4ce846f85bebc3a43f63c98
bane.shi <fengxiaotx@163.com>
2025-12-09 18:00:09 +0800

    feat:

--- src/search-toolbar/search-toolbar.tsx
+++ src/search-toolbar/search-toolbar.tsx
@@ -1,7 +1,7 @@
 import { DownOutlined, UpOutlined } from '@ant-design/icons';
 import { Button, Col, Form, FormItemProps, Grid, Row, Space } from 'antd';
 import classNames from 'classnames';
-import { size } from 'lodash-es';
+import { size as _size } from 'lodash-es';
 import React, { FC, useEffect, useState } from 'react';
 import { useCssInJs } from '../hooks';
 import { genSearchToolbarStyle } from './styled';
@@ -78,7 +78,7 @@ const SearchToolbar: FC<SearchToolbarProps> = ({
   };
 
   const handleCompute = () => {
-    const itemSize = size(items);
+    const itemSize = _size(items);
     const rowColSize = 24 / colSpan;
     const compensateColCount = handleCompleteCompensateColCount(
       rowColSize,
